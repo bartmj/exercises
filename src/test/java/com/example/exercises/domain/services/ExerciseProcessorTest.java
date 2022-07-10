d5795f47e177de6c782de1a76021914455e1f2eb
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-07-10 13:00:25 +0200

    ExerciseProcessorTest 1 test for getAll()

--- src/test/java/com/example/exercises/domain/services/ExerciseProcessorTest.java
+++ src/test/java/com/example/exercises/domain/services/ExerciseProcessorTest.java
@@ -1,5 +1,6 @@
 package com.example.exercises.domain.services;
 
+import com.example.exercises.domain.model.Exercise;
 import com.example.exercises.domain.ports.ExerciseRepository;
 import com.example.exercises.test.tools.ExerciseTestTools;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
@@ -21,7 +27,6 @@ class ExerciseProcessorTest {
     private ExerciseProcessor exerciseService;
 
     @Test
-    @DisplayName("")
     public void saveExercise_shouldReturn_IdOf1L() throws Exception {
         var exercise = ExerciseTestTools.createValidExercise();
 
@@ -31,5 +36,19 @@ class ExerciseProcessorTest {
         assertEquals(aLong, 1L);
     }
 
+    @Test
+    public void getAll_shouldReturn_listOfDomainExercises() {
+        var exerciseList = ExerciseTestTools.createExerciseList();
 
+        when(exerciseRepository.getAll()).thenReturn(exerciseList);
+
+        var exercises = exerciseService.getAll();
+
+        assertAll(
+                () -> assertEquals(exercises.get(0).getName(), "Przysiad ze sztangą"),
+                () -> assertEquals(exercises.get(0).getEngName(), "Squat with a barbell"),
+                () -> assertEquals(exercises.get(0).getMainMuscleGroup(), new HashSet<>(Arrays.asList("czworogłowy uda"))),
+                () -> assertEquals(exercises.get(0).getSupportMuscleGroup(), new HashSet<>(Arrays.asList("dwugłowy uda", "pośladkowy")))
+        );
+    }
 }
