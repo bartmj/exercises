02210c3e09588cdbaff3a85741e4845313e9a8dd
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-07-13 15:05:07 +0200

    training

---
+++ src/main/java/com/example/training/domain/model/ExerciseSet.java
@@ -0,0 +1,10 @@
+package com.example.training.domain.model;
+
+import com.example.exercises.domain.model.Exercise;
+
+public class ExerciseSet {
+
+    private Exercise exercise;
+    private int reps;
+    private int weight;
+}
---
+++ src/main/java/com/example/training/domain/model/TrainingDay.java
@@ -0,0 +1,8 @@
+package com.example.training.domain.model;
+
+import java.util.ArrayList;
+
+public class TrainingDay {
+
+    private ArrayList<ExerciseSet> exerciseSets;
+}
