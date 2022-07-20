a1bc1c11b9d9d569dc731ac43c52d16c3b66a73f
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-07-20 16:54:05 +0200

    query with user id only

--- src/main/java/com/example/exercises/adapters/persistence/repository/JpaExerciseSetRepository.java
+++ src/main/java/com/example/exercises/adapters/persistence/repository/JpaExerciseSetRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface JpaExerciseSetRepository extends JpaRepository<ExerciseSetEntity, Long> {
 
+    @Query(value = "SELECT e.* FROM exerciseset e where e.id = :userId", nativeQuery = true)
     List<ExerciseSetEntity> findAllById(Long userId);
 }
